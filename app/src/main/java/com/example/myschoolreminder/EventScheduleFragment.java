f7e74730b6fa8c07f312b10ca86702332bb9f913
Lucie Moulin <lucie.moulin@eduvaud.ch>
2019-11-27 15:52:06 +0100

    Added the event creation activity and schedule fragment

---
+++ app/src/main/java/com/example/myschoolreminder/EventScheduleFragment.java
@@ -0,0 +1,115 @@
+package com.example.myschoolreminder;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import com.example.myschoolreminder.Objects.Repetition;
+import com.example.myschoolreminder.Objects.RepetitionType;
+
+import java.util.ArrayList;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Activities that contain this fragment must implement the
+ * {@link EventScheduleFragment.OnFragmentInteractionListener} interface
+ * to handle interaction events.
+ * Use the {@link EventScheduleFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class EventScheduleFragment extends Fragment {
+    private OnFragmentInteractionListener mListener;
+
+    public EventScheduleFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @return A new instance of fragment EventScheduleFragment.
+     */
+    public static EventScheduleFragment newInstance() {
+        EventScheduleFragment fragment = new EventScheduleFragment();
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState){
+        //Gets the repetition types
+        RepetitionType[] rawTypes = RepetitionType.values();
+        CharSequence[] types = new CharSequence[RepetitionType.values().length];
+
+        for(int i = 0; i < RepetitionType.values().length; i++){
+            types[i] = rawTypes[i].getName();
+        }
+
+        //Gets the spinner
+        Spinner spinner = view.findViewById(R.id.spinnerRepetitionType);
+
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this.getContext(), R.array.repetitionTypes, android.R.layout.simple_spinner_item);
+
+        // Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        adapter.addAll(types);
+
+        // Apply the adapter to the spinner
+        spinner.setAdapter(adapter);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_event_schedule, container, false);
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if (context instanceof OnFragmentInteractionListener) {
+            mListener = (OnFragmentInteractionListener) context;
+        } else {
+            throw new RuntimeException(context.toString()
+                    + " must implement OnFragmentInteractionListener");
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+
+    /**
+     * This interface must be implemented by activities that contain this
+     * fragment to allow an interaction in this fragment to be communicated
+     * to the activity and potentially other fragments contained in that
+     * activity.
+     * <p>
+     * See the Android Training lesson <a href=
+     * "http://developer.android.com/training/basics/fragments/communicating.html"
+     * >Communicating with Other Fragments</a> for more information.
+     */
+    public interface OnFragmentInteractionListener {
+        void onFragmentInteraction();
+    }
+}
